1e96d4de03e126bb24e913f304d986386edf5a61
Uwe Kamper <me@uwekamper.de>
2012-04-07 18:17:34 +0200

    View funktioniert und wird gerendert

--- media/js/skills.js
+++ media/js/skills.js
@@ -1,59 +1,110 @@
-$(function(){
-
-	var Skill = Backbone.Model.extend({
-		defaults: {
-			name: null,
-			description: null,
-			level: null,
-			love: null
-		}
-	});
-
-	var Skillset = Backbone.Collection.extend({
-		model: Skill,
-		url: '/skills/skills.json',
-		currentStatus : function(status) {
-			return _(this.filter(function(data) {
-			  	return data.get("completed") == status;
-			}));
-		},
-		search : function(letters){
-			if(letters == "") return this;
-
-			var pattern = new RegExp(letters,"gi");
-			return _(this.filter(function(data) {
-			  	return pattern.test(data.get("name"));
-			}));
-		}
-	});
-
-	var mySkillset = new Skillset();
-
-	var SkillsContainerView = Backbone.View.extend({
-		initialize: function(){
-			// this.on("reset", this.render(), this);
-		},
-		events: {
-			"keyup #skills_search": "search"
-		},
-		render: function(){
-			this.renderList(this.collection);
-			return this;
-		},
-		renderList: function(skills) {
-			$("#skills_list").html("");
-			
-			var template = _.template( $("#skills_template").html(), {skills: skills.toJSON()} );
-			$("#skills_list").html( template );
-			return this;
-		},
-		search: function(e) {
-			var searchterm = $("#searchTask").val();
-			this.renderList(this.collection.search(searchterm));
-		}
-	});
-
-	var search_view = new SkillsContainerView({ el: $("#skills_container"), collection:mySkillset });
-	
-	mySkillset.fetch( {success: function() {search_view.render();} } );
+var Skill = Backbone.Model.extend({
+	defaults: {
+		name: null,
+		description: null,
+		level: null,
+		love: null
+	}
 });
+
+var SkillList = Backbone.Collection.extend({
+	model: Skill,
+	url: '/skills/skills.json',
+
+	search : function(letters){
+		if(letters == "") return this;
+
+		var pattern = new RegExp(letters,"gi");
+		return _(this.filter(function(data) {
+		  	return pattern.test(data.get("name"));
+		}));
+	}
+});
+
+var Skills = new SkillList();
+
+// var SkillsAppModel = Backbone.Model.extend({
+// 	initialize: function() {
+// 		this.skills = new Skillset();
+// 	},
+// 	fetch: function(args) {
+// 		this.skills.fetch(args);
+// 	}
+// });
+
+var SkillView = Backbone.View.extend({
+	tagName: "li",
+	
+	template: _.template( $("#skill_template").html() ),
+	
+	events: {
+		"click .skill_name": "handleNameClick"
+	},
+	
+	initialize: function(){
+		this.model.bind('change', this.render, this);
+	},
+	
+	render: function(){
+		this.$el.html(this.template(this.model.toJSON()));
+		return this;
+	},
+	
+	handleNameClick: function() {
+		alert("Name clicked!");
+	}
+	
+});
+
+var SkillListView = Backbone.View.extend({
+	el: $("skills_list"),
+	
+	initialize: function() {
+		Skills.bind('add', this.addOne, this);
+		Skills.bind('reset', this.addAll, this);
+		Skills.bind('all', this.render, this);
+		
+		Skills.fetch();
+		
+	},
+	
+	addOne: function(skill) {
+		var view = new SkillView({model: skill});
+		this.$("#skills_list").append(view.render().el);
+	},
+	
+	addAll: function() {
+		Skills.each(this.addOne);
+	},
+	
+	render: function() {
+		
+	},
+	
+	search: function(e) {
+		var searchterm = $("#searchTask").val();
+		this.renderList(this.collection.search(searchterm));
+	}
+});
+
+// var SkillsAppController = {
+// 	init: function(spec){
+// 		// default values for the configuration
+// 		this.config = {
+// 			connect: true
+// 		};
+// 		
+// 		_.extend(this.config, spec);
+// 		
+// 		this.model = new Skillset();
+// 		this.view = new SkillListView({model: this.model});
+// 		
+// 		return this;
+// 	},
+// 	run: function() {
+// 		this.model.fetch( {success: function() {this.view.render();}} );
+// 	}
+// }
+//var search_view = new SkillsContainerView({ el: $("#skills_container"), collection:mySkillset });
+
+// mySkillset.fetch( {success: function() {search_view.render();} } );
